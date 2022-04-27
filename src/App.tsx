0f3216448a582a6238a009ab8b8ec8ea3ee73af6
Roger Urich <roger@itsovertime.com>
2022-04-27 13:01:42 -0700

    boilerplate react template

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Main } from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl font-bold sm:my-10 my-3 w-full mt-10 font-mono">
-          <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-12 w-12 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-          </svg>
-          My Library
-        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-
-      <div className="sm:max-w-xl md:max-w-3xl sm:w-3/4 mx-5 sm:mx-auto">
-        <Main />
-      </div>
     </div>
   );
 }
